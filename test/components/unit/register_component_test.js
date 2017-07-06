c1ee70b7b7ed142cc7e3090cdaf1474566931dbf
Jacob Stoebel <jstoebel@gmail.com>
2017-07-06 18:37:10 -0400

    add test for register component

--- test/components/unit/register_component_test.js
+++ test/components/unit/register_component_test.js
@@ -1,31 +1,29 @@
-/* global describe it */
+/* global describe it beforeEach */
 import React from 'react';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
-import {Field} from 'redux-form'
-import {Alert} from 'react-bootstrap';
+import {Field} from 'redux-form';
 import sinon from 'sinon';
-import Login from '../../../src/components/ui/Login';
+import Register from '../../../src/components/ui/Register';
 
-describe('<Login/>', () => {
+describe('<Register/>', () => {
   let wrapper;
-  let handleSubmit = sinon.spy()
-  let loginUser = sinon.spy()
+  let handleSubmit = sinon.spy();
   beforeEach((done) => {
-    wrapper = shallow(<Login 
+    wrapper = shallow(<Register
       handleSubmit={handleSubmit}
-      loginUser={loginUser}
     /> );
-    done()
-  })
+    done();
+  });
 
   describe('renders fields', () => {
     let fields;
     beforeEach((done) => {
-      fields = wrapper.find(Field)
+      fields = wrapper.find(Field);
+      console.log(`there are ${fields.length} fields`);
       done();
-    })
-    const fieldNames = ['email', 'password'];
+    });
+    const fieldNames = ['firstName', 'lastName', 'email', 'password'];
     fieldNames.forEach((name) => {
       it(`renders ${name}`, (done) => {
         expect(fields.find({name: name})).to.have.length(1)
@@ -39,13 +37,11 @@ describe('<Login/>', () => {
     const msg = 'Something went wrong'
     wrapper.setProps({errorMessage: msg})
     expect(
-        wrapper.find(Alert)
-          .find('span')
+        wrapper.find('div span')
           .text()
     ).to.equal(`Error! ${msg}`)
     
     done();
   })
-
   
 })
